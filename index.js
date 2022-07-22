2957f26d9ff9c4ec54ce601b692fa51443c7f76a
Bellisario <72039923+Bellisario@users.noreply.github.com>
2022-07-22 18:00:13 +0200

    noOverlap better algorithm

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const getMaxNumbers = (minimum, maximum) => maximum - minimum + 1;
+function filledSet(minimum, maximum) {
+	const set = new Set();
+	for (let i = minimum; i < maximum + 1; i++) {
+		set.add(i);
+	}
+
+	return set;
+}
 
 export default function uniqueRandom(minimum, maximum, options) {
-	const extracted = new Set();
+	const defaultSet = filledSet(minimum, maximum);
+	let toExtract = new Set(defaultSet);
 	let previousValue;
 
 	return function random() {
@@ -9,13 +17,19 @@ export default function uniqueRandom(minimum, maximum, options) {
 			(Math.random() * (maximum - minimum + 1)) + minimum
 		);
 
-		if (extracted.size === getMaxNumbers(minimum, maximum)) {
-			extracted.clear();
+		if (toExtract.size === 0) {
+			toExtract = new Set(defaultSet);
 		}
 
 		if (options?.noOverlap) {
-			previousValue = ((extracted.has(number) || previousValue === number) && random()) || (extracted.add(number) && number);
-			return previousValue;
+			const value = [...toExtract][Math.floor(Math.random() * toExtract.size)];
+			if (previousValue === value) {
+				return random();
+			}
+
+			toExtract.delete(value);
+			previousValue = value;
+			return value;
 		}
 
 		previousValue = number === previousValue && minimum !== maximum ? random() : number;
