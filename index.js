1b3eed5f58e55deb65b79fb034feec235fc05694
Bellisario <72039923+Bellisario@users.noreply.github.com>
2022-07-22 18:24:58 +0200

    noOverlap: use array instead of set

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function filledSet(minimum, maximum) {
-	const set = new Set();
+function filledArray(minimum, maximum) {
+	const array = [];
 	for (let i = minimum; i < maximum + 1; i++) {
-		set.add(i);
+		array.push(i);
 	}
 
-	return set;
+	return array;
 }
 
 export default function uniqueRandom(minimum, maximum, options) {
-	const defaultSet = filledSet(minimum, maximum);
-	let toExtract = new Set(defaultSet);
+	const defaultArray = filledArray(minimum, maximum);
+	let toExtract = [...defaultArray];
 	let previousValue;
 
 	return function random() {
@@ -17,17 +17,17 @@ export default function uniqueRandom(minimum, maximum, options) {
 			(Math.random() * (maximum - minimum + 1)) + minimum
 		);
 
-		if (toExtract.size === 0) {
-			toExtract = new Set(defaultSet);
+		if (toExtract.length === 0) {
+			toExtract = [...defaultArray];
 		}
 
 		if (options?.noOverlap) {
-			const value = [...toExtract][Math.floor(Math.random() * toExtract.size)];
+			const value = toExtract[Math.floor(Math.random() * toExtract.length)];
 			if (previousValue === value) {
 				return random();
 			}
 
-			toExtract.delete(value);
+			toExtract = toExtract.filter(item => item !== value);
 			previousValue = value;
 			return value;
 		}
