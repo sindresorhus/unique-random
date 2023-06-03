4ec089d466ce95df2a78a3072e19b3433b02bc90
Utkarsh Chauhan <52543077+BLANK00ANONYMOUS@users.noreply.github.com>
2023-06-03 23:34:07 +0530

    Used set method for unique random number generation
    Hello Sir,
    i tried to come up with set solution. In this essentially if i ask for 1800 random superheroes it gave me 1800 unique superheroes or whatever is the upper limit of superheroes list in the json file

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+const usedNumbers = new Set();
+function generateUniqueRandomNumber(min, max) {
+	let randomNum;
+	do {
+		randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+	} while (usedNumbers.has(randomNum));
+
+	usedNumbers.add(randomNum);
+
+	return randomNum;
+}
+
 export default function uniqueRandom(minimum, maximum) {
-	let previousValue;
-
-	return function random() {
-		const number = Math.floor(
-			(Math.random() * (maximum - minimum + 1)) + minimum
-		);
-
-		previousValue = number === previousValue && minimum !== maximum ? random() : number;
-
-		return previousValue;
+	return function giveRandomNumber() {
+    		const number = generateUniqueRandomNumber(minimum, maximum);
+		return number;
 	};
 }
