97dfa4854018ba51ed29061943f54dc5c9e8a353
Richie Bendall <richiebendall@gmail.com>
2024-02-29 20:04:48 +1300

    Add exhaustive sampling and rename the default export to `consecutiveUniqueRandom`

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-export default function uniqueRandom(minimum, maximum) {
+function * range(minimum, maximum) {
+	for (let number = minimum; number <= maximum; number++) {
+		yield number;
+	}
+}
+
+// Mutates the input function
+function withIteratorShortcut(function_) {
+	function_[Symbol.iterator] = function * () {
+		while (true) {
+			yield function_();
+		}
+	};
+
+	return function_;
+}
+
+export function exhaustiveUniqueRandom(minimum, maximum) {
+	let unconsumedValues = [...range(minimum, maximum)];
+
+	return withIteratorShortcut(function random() {
+		if (unconsumedValues.length == 0) {
+			unconsumedValues = [...range(minimum, maximum)];
+		}
+
+		return unconsumedValues.splice(
+			// https://github.com/sindresorhus/random-item/blob/b832d9332044f2d4f223731dc97534efd258b441/index.js#L6C9-L6C57
+			Math.floor(Math.random() * unconsumedValues.length),
+			1,
+		)
+	});
+}
+
+export function consecutiveUniqueRandom(minimum, maximum) {
 	let previousValue;
 
-	function random() {
+	return withIteratorShortcut(function random() {
 		const number = Math.floor(
 			(Math.random() * (maximum - minimum + 1)) + minimum,
 		);
@@ -9,13 +42,5 @@ export default function uniqueRandom(minimum, maximum) {
 		previousValue = (number === previousValue && minimum !== maximum) ? random() : number;
 
 		return previousValue;
-	}
-
-	random[Symbol.iterator] = function * () {
-		while (true) {
-			yield random();
-		}
-	};
-
-	return random;
+	});
 }
